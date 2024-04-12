0ed7327df919d047b7f8896c909e532be2eb51f2
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2024-04-12 22:06:38 +1000

    response to SYN in receiver (not finished)

--- receiver.py
+++ receiver.py
@@ -13,57 +13,35 @@
 import socket
 import sys
 from arg_parser import ArgParser
+from helpers import Helpers
+from enums import LogActions, SegmentType
 
-NUM_ARGS = 3  # Number of command-line arguments
-BUF_SIZE = 3  # Size of buffer for sending/receiving data
+NUM_ARGS = 4  # Number of command-line arguments
+BUF_SIZE = 1004  # Size of buffer for sending/receiving data
 
 if __name__ == "__main__":
     if len(sys.argv) != NUM_ARGS + 1:
         sys.exit(f"Usage: {sys.argv[0]} rcvr_port sender_port txt_file_received max_win")
 
-    # ================== Upasdasdasdate arguments =====================
+    # ================== Update arguments =====================
     rcvr_port = ArgParser.parse_port(sys.argv[1]) 
     sender_port = ArgParser.parse_port(sys.argv[2])
     txt_file_received = sys.argv[3]
     max_win = ArgParser.parse_max_win(sys.argv[4])
 
     # ================== Update socket setup =====================
+    Helpers.reset_log('receiver')
+    
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.bind(('127.0.0.1', my_port)) 
-        s.connect(('127.0.0.1', peer_port))
-        s.settimeout(wait_time)
+        s.bind(('127.0.0.1', rcvr_port))
+        s.connect(('127.0.0.1', sender_port))
 
         while True:
-            # Here we're using recvfrom() and sendto(), but we could also 
-            # connect() this UDP socket to set communication with a particular 
-            # peer. This would allow us to use send() and recv() instead, 
-            # but only communicate with one peer at a time.
-            # ====================== Update to send() and recv() ================
-            try:
-                buf = s.recv(BUF_SIZE)
-            except socket.timeout:
-                print(f"No data within {wait_time} seconds, shutting down.")
-                break
+            receive = s.recv(BUF_SIZE)
+            segmentType = int.from_bytes(receive[:2], 'big')
 
-            if len(buf) < BUF_SIZE-1:
-                print(f"recvfrom: received short message: {buf}", file=sys.stderr)
-                continue
-
-            # Packet was received, first (and only) field is multi-byte, 
-            # so need to convert from network byte order (big-endian) to 
-            # host byte order.  Then log the recv.
-            num = int.from_bytes(buf[:2], byteorder='big')
-            print(f"127.0.0.1:{peer_port}: rcv: {num:>5}")
-
-            # Determine whether the number is odd or even, and append the 
-            # result (as a single byte) to the buffer.
-            odd = num % 2
-            buf += odd.to_bytes(1, byteorder='big')
-
-            # Log the send and send the reply.
-            print(f"127.0.0.1:{peer_port}: snd: {num:>5} {'odd' if odd else 'even'}")
-            if (s.send(buf) != len(buf)):
-                print(f"sendto: partial/failed send, message: {buf}", file=sys.stderr)
-                continue
-
+            if segmentType == SegmentType.SYN:
+                seqno = int.from_bytes(receive[2:4], 'big')
+                # For SYN segment, add 1 to seqno
+                seqno += 1
     sys.exit(0)
