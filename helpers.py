c52e17401b9d06d53f5f58f667c72411d7ee2ef1
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2024-04-13 01:07:15 +1000

    re-implement helpers

--- helpers.py
+++ helpers.py
@@ -1,12 +1,27 @@
 # General helper functions
+MAX_SEQNO = 2**16
 class Helpers:
-    def log_message(user: str, action: str, time: float, segment_type: str, seqno: int, num_bytes: int):
+    @staticmethod
+    def log_message(user: str, action: str, time: float, segment_type: str, seqno: int, num_bytes: int) -> None:
         log_file = f"{user}_log.txt"
         with open(log_file, 'a') as file:
             file.write(f"{action} {time} {segment_type} {seqno} {num_bytes}\n")
+        return
     
-    def reset_log(user):
+    @staticmethod
+    def reset_log(user: str) -> int:
         log_file = f"{user}_log.txt"
         with open(log_file, 'w') as file:
             pass
         return
+    
+    @staticmethod
+    def add_seqno(seqno: int, amount: int) -> int:
+        '''
+            Args:
+                seqno (int): current sequence number
+                amount(int): amount of bytes added to seqno
+            Returns:
+                int: resulted seqno after addition (and MOD MAX_SEQNO) 
+        '''
+        return (seqno + amount) % MAX_SEQNO
