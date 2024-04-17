a34e10b0c9bc0211308fba3be1cdf582f0fd3d7c
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2024-04-17 21:25:53 +1000

    move prototypes of sender to sender_prototypes

--- sender.py
+++ sender.py
@@ -18,37 +18,7 @@ from arg_parser import ArgParser
 from helpers import Helpers
 from states import States
 from stp_helpers import Stp
-
-NUM_ARGS  = 7  # Number of command-line arguments
-BUF_SIZE  = 4  # Size of buffer for receiving messages
-MAX_SEQNO = 2**16 # Maximum sequence number
-MSS = 1000     # Maximum segment size
-@dataclass
-class Control:
-    """Control block: parameters for the sender program."""
-    # ================== Update arguments =====================
-    sender_port: int    # Port number of the sender
-    rcvr_port: int      # Port number of the receiver
-    max_win: int        # max window size, should be the same for receiver side
-    rto: float          # retransmission time for a socket
-    seqno: int          # sequence number of sender socket
-    file_name: str      # name of file being sent
-    rlp: float          # probability of incoming packet being dropped
-    flp: float          # probability of sent packet being dropped
-    socket: socket.socket   # Socket for sending/receiving messages
-    is_connected: bool = False # a flag to signal successful connection or when to terminate
-    is_est_state: bool = False # a flag to signal whether our sender program is in EST state
-    start_time: float = 0.0   # time in miliseconds at first sent segment
-    timer: threading.Timer = None # A single timer associates with the EST state
-
-@dataclass 
-class SegmentControl:
-    """Segment Control block: manages data segments related info"""
-    segments: list      # List of 1000 bytes max segments from file
-    send_base: int = 0  # The index of the oldest unACKed segment
-    end: int = 0        # The end index of current sliding window on segments[]
-    seqno_map: dict     # A dictionary to map seqno to its corresponding index in list
-    dupACK_cnt: int = 0 # The count of duplicate ACKed segment for fast retransmit 
+from sender_prototypes import NUM_ARGS, MAX_SEQNO, Control
 
 # =====================Update setup_socket function ========================
 def setup_socket(sender_port):
@@ -75,7 +45,8 @@ if __name__ == "__main__":
 
     # Use a fixed seed for debugging purpose, NEED TO CHANGED BEFORE SUBMITTED
     random.seed(1)  # Seed the random number generator
-    isn = random.randrange(MAX_SEQNO)
+    # isn = random.randrange(MAX_SEQNO)
+    isn = 0
 
     # Create a control block for the sender program.
     control = Control(sender_port=sender_port, rcvr_port=rcvr_port, 
