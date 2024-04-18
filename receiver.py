f2279e6131af911ab81d39a681b6a847706baa94
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2024-04-19 05:33:05 +1000

    add lru_cache class to receiver

--- receiver.py
+++ receiver.py
@@ -34,6 +34,43 @@ class Control:
     max_win:    int             # maximum window size for receiver buffer
     start_time: float = 0.0     # Time at which first packet received
 
+class LRU_Acked_Cache:
+    def __init__(self, max_queue_size: int) -> None:
+        self.acked_map: dict[int, bool] = {}
+        self.acked_queue: list[int] = []
+        self.MAX_QUEUE_SIZE = max_queue_size
+
+    def find(self, seqno: int) -> bool:
+        '''
+            Args:
+                seqno (int): sequence number of a segment
+            Returns:
+                bool: returns True if recently received segment of seqno, False otherwise.
+        '''
+        return self.acked_map.get(seqno, False) != False
+
+    def add_seqno(self, seqno: int) -> None:
+        '''
+            Add a recently received seqno into cache. If number of seqno saved exceeds (MAX_WIN/1000)*2, 
+            pop the first elem out and delete it from dictionary.
+            The choice of (MAX_WIN/1000)*2 might not be the most optimal number, but I just play safe here.
+            Didn't check for duplicate as I assume it's relatively rare for duplicates to occur.
+
+            Args:
+                seqno (int): sequence number of a segment
+            Returns:
+                None
+        '''
+        self.acked_map[seqno] = True
+        self.acked_queue.append(seqno)
+
+        if len(self.acked_queue) == self.MAX_QUEUE_SIZE:
+            lru_seqno = self.acked_queue.pop(0)
+            self.acked_map.pop(lru_seqno)
+
+        return
+    
+
 @dataclass
 class Buffer:
     """Control block: parameters for the receiver buffer."""
@@ -42,6 +79,7 @@ class Buffer:
     expct_seqno:int     # The expected sequence number when receive next packet
     index:      int     # The index where the next in-order packet lives in buffer
     max_size:   int     # The maximum size of buffer
+    lru_seqno: LRU_Acked_Cache # a class to keep track of recently received Acked segments
 
 if __name__ == "__main__":
     if len(sys.argv) != NUM_ARGS + 1:
@@ -68,7 +106,7 @@ if __name__ == "__main__":
             
             is_first_segment = True
             control.start_time = Helpers.get_time_mls()
-
+            i = 0 #debug purpose
             while True:
                 receive = s.recv(BUF_SIZE)
                 segmentType, seqno, data = Stp.extract_stp_segment(receive)
@@ -93,11 +131,11 @@ if __name__ == "__main__":
                     Helpers.log_message('receiver', LogActions.SEND, control.start_time, SegmentType.ACK, seqno, 0)
 
                     # Initialize buffer
-                    buff = Buffer([None] * max_buff_size, seqno, 0, max_buff_size)
+                    buff = Buffer([None] * max_buff_size, seqno, 0, max_buff_size, LRU_Acked_Cache(max_buff_size * 2))
                 elif segmentType == SegmentType.DATA:
                     # Need to handle negative MOD
                     print('receieve DATA from sender')
-
+                    print(seqno, buff.expct_seqno)
                     # If in-order packet, place it to buffer[index]
                     if seqno == buff.expct_seqno:
                         if buff.buffer[buff.index] != None: 
@@ -107,9 +145,10 @@ if __name__ == "__main__":
                         # Iterate thru buffer to write all in-order data
                         while buff.buffer[buff.index] != None:
                             buffered_rcv_data = buff.buffer[buff.index]
+                            buff.lru_seqno.add_seqno(buff.expct_seqno)
+                            
                             current_seqno = (buff.expct_seqno + len(buffered_rcv_data)) % MAX_SEQNO
 
-                            # Write data to output file
                             f.write(buffered_rcv_data.decode())
 
                             # Re-empty this position in buffer
@@ -117,8 +156,8 @@ if __name__ == "__main__":
 
                             buff.expct_seqno = current_seqno
                             buff.index = (buff.index + 1) % buff.max_size
-                    else:
-                        # If this is an out-of-order packet, place it at somewhere with respect to 
+                    elif not buff.lru_seqno.find(seqno):
+                        # If this is an out-of-order packet (by checking whether it exists in our lru cache), place it at somewhere with respect to 
                         # the position of the expected in-order data
                         
                         # For example, we are expecting packet seqno = 1000 at index 1,
@@ -141,18 +180,23 @@ if __name__ == "__main__":
                             #   4000       1000
                             seqno_diff += MAX_SEQNO
                         
-                        final_index = buff.index + (seqno_diff // MSS)
-
+                        final_index = (buff.index + (seqno_diff // MSS)) % buff.max_size
+                        print(final_index)
                         if buff.buffer[final_index] != None:
                             raise Exception('buffer[final_index] is not empty')
 
                         buff.buffer[final_index] = data
+                    else:
+                        # Getting to this means that we received an already ACKED segment, so we don't do anything here
+                        pass
 
                     # Send back an ACK segment
                     ack_segment = Stp.create_stp_segment(SegmentType.ACK, buff.expct_seqno)
                     s.send(ack_segment)
-                    Helpers.log_message('receiver', LogActions.SEND, control.start_time, SegmentType.ACK, current_seqno, 0)
+                    Helpers.log_message('receiver', LogActions.SEND, control.start_time, SegmentType.ACK, buff.expct_seqno, 0)
         sys.exit(0)
     except Exception as e:
         print(e)
         sys.exit(1)
+    finally:
+        f.close()
